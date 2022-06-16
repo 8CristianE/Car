a51da838db866599811baa3829a7dbd030e3beec
cristianespinosa7621@gmail.com <email@example.com>
2022-06-15 18:47:54 -0700

    initial commit

---
+++ CarProject/Car.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace CarProject
+{
+	public class Car
+	{
+		public Car()
+		{
+		}
+		public string Make;
+		public string Model;
+		public int Year;
+        
+	}
+}
+
---
+++ CarProject/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace CarProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car NewCar = new Car();
+
+            NewCar.Make = "Ford";
+            Console.WriteLine($"I just got a new car made by {NewCar.Make}");
+
+            NewCar.Model = "Mustang";
+            Console.WriteLine($"It's a {NewCar.Make} {NewCar.Model}");
+
+            NewCar.Year = 2018;
+            Console.WriteLine($"It's a year {NewCar.Year}");
+
+        }
+    }
+}
+
